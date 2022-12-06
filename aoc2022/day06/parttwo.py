92b52d1c18b3abc6f78206563b3fc9ac036c4edf
Kai Tinkess <kaitinkess@gmail.com>
2022-12-06 00:13:20 -0500

    day 6 comeback

---
+++ aoc2022/day06/parttwo.py
@@ -0,0 +1,21 @@
+import string
+
+lines = []
+debug = 0
+
+with open('test.txt' if debug else 'input.txt') as file:
+    lines = file.readlines()
+
+input = list(lines[0])
+
+for i, val in enumerate(input):
+    if i < 14: continue
+
+    count = dict.fromkeys(string.ascii_lowercase, 0)
+    for char in input[i-14:i]:
+        count[char] += 1
+
+    if max(count.values()) <= 1:
+        print(i)
+        break
+    
